2006b4dea7232d34f8fa1ceda1f25719a9308be2
Xin Yang <65144493+vegetable-yx@users.noreply.github.com>
2022-08-15 15:51:49 +0800

    Update update_tables.sql

--- sql/update_tables.sql
+++ sql/update_tables.sql
@@ -1,7 +1,6 @@
 alter table EMPLOYEE add BIRTHDAY DATE;
 
-alter table MANAGE add REPAIR INT;
-alter table MANAGE add constraint fk_repair foreign key(REPAIR) references REPAIR(REPAIR_ID);
-
 alter table DISHES add VIDEO VARCHAR(50);
 alter table DISHORDERLIST add REMARK VARCHAR (150);
+
+alter table SALARY drop constraint SYS_C0011079;
